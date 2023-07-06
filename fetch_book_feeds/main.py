d992240a2bfe38a1f397aed50b1fc34ad4cf1ef2
takada-at <takada-at@klab.com>
2023-07-06 21:33:28 +0900

    cache

--- fetch_book_feeds/main.py
+++ fetch_book_feeds/main.py
@@ -3,6 +3,7 @@ from google.cloud import storage
 from typing import Dict, NamedTuple
 import feedparser
 import functions_framework
+import google.api_core.exceptions
 import gzip
 import json
 import os
@@ -30,15 +31,27 @@ class HanmotoData(NamedTuple):
         return dict(id=self.id, raw_title=self.raw_title, title=self.title,
                     authors=self.authors, publisher=self.publisher,
                     publish_date=self.publish_date, link=self.link,
-                    isbn=self.isbn, description=description, keyword=keyword, c_code=c_code,
+                    isbn=self.isbn, description=description, keywords=keyword, c_code=c_code,
                     author_data=author_data)
 
 
-def upload_gcs(bucket_name, path, local_path):
+def upload_gcs(bucket_name: str, path: str, local_path):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(path)
+    blob.content_encoding = "gzip"
     blob.upload_from_filename(local_path)
+
+
+def download_gcs(bucket_name: str, path: str) -> str:
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(path)
+    blob.content_encoding = "gzip"
+    try:
+        return blob.download_as_text()
+    except google.api_core.exceptions.NotFound:
+        return ""
 
 
 def parse_openbd(record):
@@ -136,18 +149,47 @@ def fetch_feed(url):
         yield from books
 
 
+def get_new_book_cache(bucket_name: str, path: str, today: str):
+    new_book_cache = download_gcs(bucket_name, path)
+    return_values = []
+    for line in new_book_cache.split("\n"):
+        line = line.strip()
+        if not line:
+            continue
+        isbn, date = line.split("\t")
+        if date < today:
+            continue
+        return_values.append((isbn, date))
+    return return_values
+
+
 def fetch_and_save(url: str, bucket_name: str):
+    cache_data_path = "cache/new_book_cache.txt.gz"
     today = datetime.now().date().isoformat()
-    tmp = tempfile.NamedTemporaryFile("wb")
+    new_book_cache = get_new_book_cache(bucket_name, cache_data_path, today)
+    new_book_set = set([d[0] for d in new_book_cache])
+    feed_data = tempfile.NamedTemporaryFile("wb")
+    cache_data = tempfile.NamedTemporaryFile("wb")
     count = 0
-    with gzip.open(tmp.name, "wb") as f:
-        for b in fetch_feed(url):
-            f.write((json.dumps(b) + "\n").encode("utf-8"))
-            count += 1
-    tmp.flush()
-    tmp.seek(0)
-    remote_path = f"new_book/date={today}/hanmoto.json.gz"
-    upload_gcs(bucket_name, remote_path, tmp.name)
+    with gzip.open(feed_data.name, "wb") as f:
+        with gzip.open(cache_data.name, "wb") as cf:
+            for cache in new_book_cache:
+                cf.write((cache[0] + "\t" + cache[1] + "\n").encode("utf-8"))
+            for b in fetch_feed(url):
+                if b["isbn"] in new_book_set:
+                    continue
+                cf.write((b["isbn"] + "\t" + b["publish_date"] + "\n").encode("utf-8"))
+                f.write((json.dumps(b) + "\n").encode("utf-8"))
+                count += 1
+    feed_data.flush()
+    feed_data.seek(0)
+    cache_data.flush()
+    cache_data.seek(0)
+    remote_path = f"new_books/date={today}/hanmoto.jsonl.gz"
+    if count > 0:
+        upload_gcs(bucket_name, remote_path, feed_data.name)
+    upload_gcs(bucket_name, cache_data_path, cache_data.name)
+    print(dict(date=today, count=count))
     return dict(count=count, date=today)
 
 
@@ -155,5 +197,6 @@ def fetch_and_save(url: str, bucket_name: str):
 def handle_request(request):
     base_url = os.environ.get("BASE_URL")
     bucket_name = os.environ.get("BUCKET_NAME")
+    print(f"base_url: {base_url}")
     result = fetch_and_save(base_url, bucket_name)
     return dict(result="ok", **result)
--- terraform/resource/main.tf
+++ terraform/resource/main.tf
@@ -11,6 +11,14 @@ resource "google_service_account" "default" {
 resource "google_storage_bucket" "data_storage" {
   name     = "td-book-storage"
   location = var.region
+  lifecycle_rule {
+    condition {
+      age = 50
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
 
 resource "google_storage_bucket_iam_binding" "binding" {
@@ -23,27 +31,24 @@ resource "google_storage_bucket_iam_binding" "binding" {
 
 data "archive_file" "sample_function" {
   type        = "zip"
-  source_dir  = "${local.root_path}fetch_feeds"
-  output_path = "${local.root_path}/output/fetch_feeds.zip"
+  source_dir  = "${local.root_path}fetch_book_feeds"
+  output_path = "${local.root_path}/output/fetch_book_feeds.zip"
 }
 
 resource "google_storage_bucket_object" "object" {
-  name   = "fetch_feeds.zip"
+  name   = "fetch_book_feeds.zip"
   bucket = google_storage_bucket.data_storage.name
-  source = "fetch_feeds.zip"
+  source = data.archive_file.sample_function.output_path
+
 }
 
 resource "google_cloudfunctions2_function" "function" {
-  name = "fetch-feeds"
+  name = "fetch-book-feeds"
   location = var.region
 
   build_config {
     runtime = "python311"
     entry_point = "handle_request"
-    environment_variables = {
-        BASE_URL = "https://www.hanmoto.com/ci/bd/search/sdate/today/edate/+1month/order/desc/vw/rss20"
-        BUCKET = google_storage_bucket.data_storage.name
-    }
     source {
       storage_source {
         bucket = google_storage_bucket.data_storage.name
@@ -56,6 +61,10 @@ resource "google_cloudfunctions2_function" "function" {
     max_instance_count  = 1
     max_instance_request_concurrency = 1
     timeout_seconds     = 120
+    environment_variables = {
+        BASE_URL = "https://www.hanmoto.com/ci/bd/search/sdate/today/edate/+1month/order/desc/vw/rss20"
+        BUCKET_NAME = google_storage_bucket.data_storage.name
+    }
   }
 }
 
@@ -67,4 +76,43 @@ resource "google_cloud_run_service_iam_binding" "binding" {
   members = [
     "serviceAccount:${google_service_account.default.email}"
   ]
+}
+
+resource "google_cloud_scheduler_job" "job" {
+  name             = "fetch-book-feeds-daily"
+  description      = "fetch-book-feeds-daily"
+  schedule         = "14 0 * * *"
+  time_zone        = "Asia/Tokyo"
+
+  http_target {
+    http_method = "GET"
+    uri         = google_cloudfunctions2_function.function.service_config[0].uri
+
+    oidc_token {
+      service_account_email = google_service_account.default.email
+      audience = google_cloudfunctions2_function.function.service_config[0].uri
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "book_feed"
+  location                    = "US"
+}
+
+resource google_bigquery_table table {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id = "external_new_books"
+  external_data_configuration {
+    source_format = "NEWLINE_DELIMITED_JSON"
+    compression = "GZIP"
+    autodetect    = true
+    schema = file("${local.root_path}/terraform/scripts/new_books.json")
+    hive_partitioning_options {
+      mode = "CUSTOM"
+      source_uri_prefix = "gs://${google_storage_bucket.data_storage.name}/new_books/{date:DATE}/"
+      require_partition_filter = false
+    }
+    source_uris = ["gs://${google_storage_bucket.data_storage.name}/new_books/*"]
+  }
 }
