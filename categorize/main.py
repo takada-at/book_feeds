195d77bd04c88db16c0bf9282ceba5ed39bbb201
Atsushi Takada <yoru.hikaru@gmail.com>
2023-07-09 00:47:43 +0900

    add categorize

---
+++ categorize/main.py
@@ -0,0 +1,134 @@
+from datetime import date, datetime, timedelta
+from google.cloud import bigquery
+from google.cloud import secretmanager
+from google.cloud import storage
+import functions_framework
+import gzip
+import openai
+import os
+import tempfile
+
+
+PROJECT_NAME = os.environ["PROJECT_NAME"]
+bigquery_client = bigquery.Client(PROJECT_NAME)
+secret_manager_client = secretmanager.SecretManagerServiceClient()
+storage_client = storage.Client(project=PROJECT_NAME)
+SQL = """SELECT isbn, raw_title, authors, title, publisher, description
+FROM book_feed.external_new_books
+WHERE
+  SUBSTR(c_code, 1, 1)!="9" AND SUBSTR(c_code, 3, 2) IN ("93", "97") AND description != ""
+  AND date=@date
+"""
+
+def upload_gcs(bucket_name: str, path: str, local_path):
+    project = os.environ["PROJECT_NAME"]
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(path)
+    blob.content_encoding = "gzip"
+    blob.upload_from_filename(local_path)
+
+
+def fetch(target_date: date):
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("date", "DATE", target_date)
+        ]
+    )
+    return bigquery_client.query(SQL, job_config=job_config).to_dataframe()
+
+
+def do_openai_api(df):
+    system_prompt = """タスク
+- これらの小説を以下のカテゴリーに分類しなさい。
+
+カテゴリは以下から選びなさい。
+ミステリ、ライトノベル、ホラー、SF、ファンタジー、時代小説、児童向け、恋愛小説、官能小説、純文学、その他
+
+## OUTPUT FORMAT
+以下のフォーマット以外のものは出力しないでください。
+
+<num1>. <category1>
+<num2>. <category2>
+"""
+    lines = []
+    for i, row in df.iterrows():
+        author = row["authors"]
+        title = row["title"]
+        publisher = row["publisher"]
+        description = row["description"]
+        line = f"{i + 1}. {author}『{title}』{publisher}"
+        line += f"\n    {description}"
+        lines.append(line)
+    books = "\n".join(lines)
+    prompt = f"# BOOKS\n{books}"
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": prompt}
+    ]
+
+    # completion = openai.ChatCompletion.create(model="gpt-4", messages=prompt)
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", temperature=0, timeout=60,
+                                              messages=messages)
+    return completion["choices"][0]["message"]["content"]
+
+
+def categorize(df):
+    unit = 8
+    all_result = []
+    for i in range(0, len(df), unit):
+        target = df[i:i+unit]
+        result = do_openai_api(target)
+        all_result += check_result(result, target)
+    return all_result
+
+
+def check_result(result, target):
+    return_values = []
+    for line in result.split("\n"):
+        line = line.strip()
+        if not line:
+            continue
+        return_values.append(line.split(".")[-1])
+    assert len(return_values) == len(target)
+    return return_values
+
+
+def fetch_secret_version(key):
+    name = f"projects/{PROJECT_NAME}/secrets/{key}/versions/latest"
+    response = secret_manager_client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
+
+def categorize_date(target_date: date, bucket_name: str):
+    date_str = target_date.isoformat()
+    api_key = open(os.environ["SECRET_KEY_PATH"]).read()
+    openai.api_key = api_key
+    df = fetch(target_date)
+    if len(df) == 0:
+        print("no data")
+        return dict(count=0, date=date_str)
+    result = categorize(df)
+    df["book_type"] = "novel"
+    df["genre"] = result
+    df = df[["isbn", "raw_title", "book_type", "genre"]]
+    tmp = tempfile.NamedTemporaryFile("wb")
+    # df.to_csv("result.csv", index=False)
+    with gzip.open(tmp.name, "wb") as fp:
+        df.to_json(fp, orient="records", lines=True)
+    tmp.flush()
+    tmp.seek(0)
+    remote_path = f"categorized/date={date_str}/novel.jsonl.gz"
+    upload_gcs(bucket_name, remote_path, tmp.name)
+    return dict(count=len(result), date=date_str)
+
+
+
+@functions_framework.http
+def handle_request(request):
+    bucket_name = os.environ.get("BUCKET_NAME")
+    json_data = request.get_json()
+    print(json_data)
+    days = json_data.get("days", 0)
+    target_date = datetime.today().date() + timedelta(days=days)
+    result = categorize_date(target_date, bucket_name)
+    return dict(result="ok", **result)
--- terraform/resource/main.tf
+++ terraform/resource/main.tf
@@ -8,6 +8,15 @@ resource "google_service_account" "default" {
   display_name = "Book Service Account"
 }
 
+resource "google_project_iam_binding" "bigquery" {
+  project = var.project_name
+  role = "roles/bigquery.jobUser"
+  members = [
+    "serviceAccount:${google_service_account.default.email}"
+  ]
+  
+}
+
 resource "google_storage_bucket" "data_storage" {
   name     = "td-book-storage"
   location = var.region
@@ -27,6 +36,14 @@ resource "google_storage_bucket_iam_binding" "binding" {
     "serviceAccount:${google_service_account.default.email}"
   ]
   role    = "roles/storage.objectAdmin"
+}
+
+resource "google_storage_bucket_iam_binding" "binding2" {
+  bucket  = google_storage_bucket.data_storage.name
+  members = [
+    "serviceAccount:${google_service_account.default.email}"
+  ]
+  role    = "roles/storage.legacyBucketReader"
 }
 
 data "archive_file" "sample_function" {
@@ -60,6 +77,7 @@ resource "google_cloudfunctions2_function" "function" {
   service_config {
     max_instance_count  = 1
     max_instance_request_concurrency = 1
+    service_account_email = google_service_account.default.email
     timeout_seconds     = 120
     environment_variables = {
         PROJECT_NAME = var.project_name
@@ -127,6 +145,7 @@ resource "google_bigquery_dataset" "dataset" {
 
 resource google_bigquery_table table {
   dataset_id = google_bigquery_dataset.dataset.dataset_id
+  deletion_protection=false
   table_id = "external_new_books"
   external_data_configuration {
     source_format = "NEWLINE_DELIMITED_JSON"
@@ -139,5 +158,146 @@ resource google_bigquery_table table {
       require_partition_filter = false
     }
     source_uris = ["gs://${google_storage_bucket.data_storage.name}/new_books/*"]
+  }
+}
+
+resource google_bigquery_table table2 {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  deletion_protection=false
+  table_id = "external_categorized"
+  external_data_configuration {
+    source_format = "NEWLINE_DELIMITED_JSON"
+    compression = "GZIP"
+    autodetect    = true
+    schema = file("${local.root_path}/terraform/scripts/categorized.json")
+    hive_partitioning_options {
+      mode = "CUSTOM"
+      source_uri_prefix = "gs://${google_storage_bucket.data_storage.name}/categorized/{date:DATE}/"
+      require_partition_filter = false
+    }
+    source_uris = ["gs://${google_storage_bucket.data_storage.name}/categorized/*"]
+  }
+}
+
+data "archive_file" "categorize" {
+  type        = "zip"
+  source_dir  = "${local.root_path}categorize"
+  output_path = "${local.root_path}/output/categorize.zip"
+}
+
+resource "google_storage_bucket_object" "categorize" {
+  name   = "categorize.zip"
+  bucket = google_storage_bucket.data_storage.name
+  source = data.archive_file.categorize.output_path
+
+}
+
+resource "google_cloudfunctions2_function" "categorize" {
+  name = "categorize-book-feeds"
+  location = var.region
+
+  build_config {
+    runtime = "python311"
+    entry_point = "handle_request"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.data_storage.name
+        object = google_storage_bucket_object.categorize.name
+      }
+    }
+  }
+
+
+  service_config {
+    max_instance_count  = 1
+    max_instance_request_concurrency = 1
+    timeout_seconds     = 480
+    available_memory    = "512M"
+    service_account_email = google_service_account.default.email
+    secret_volumes {
+      mount_path = "/etc/secrets"
+      project_id = var.project_name
+      secret     = data.google_secret_manager_secret.openai.secret_id
+    }    
+    environment_variables = {
+        PROJECT_NAME = var.project_name
+        SECRET_KEY_PATH = "/etc/secrets/openai_api_key"
+        BUCKET_NAME = google_storage_bucket.data_storage.name
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_binding" "categorize" {
+  project = var.project_name
+  service = google_cloudfunctions2_function.categorize.name
+  location = google_cloudfunctions2_function.categorize.location
+  role = "roles/run.invoker"
+  members = [
+    "serviceAccount:${google_service_account.default.email}"
+  ]
+}
+
+resource "google_bigquery_table_iam_binding" "binding" {
+  project = google_bigquery_table.table.project
+  dataset_id = google_bigquery_table.table.dataset_id
+  table_id = google_bigquery_table.table.table_id
+  role = "roles/bigquery.dataViewer"
+  members = [
+    "serviceAccount:${google_service_account.default.email}"
+  ]
+}
+
+data "google_secret_manager_secret" "openai" {
+  secret_id = "openai_api_key"
+}
+
+resource "google_secret_manager_secret_iam_binding" "member" {
+  project = var.project_name
+  secret_id = data.google_secret_manager_secret.openai.secret_id
+  role = "roles/secretmanager.secretAccessor"
+  members = [
+    "serviceAccount:${google_service_account.default.email}"
+  ]
+}
+
+resource "google_cloud_scheduler_job" "job_categorize1" {
+  name             = "categorize-book-feeds-daily1"
+  description      = "categorize-book-feeds-daily1"
+  schedule         = "34 0 * * *"
+  time_zone        = "Asia/Tokyo"
+
+  http_target {
+    http_method = "POST"
+    uri         = google_cloudfunctions2_function.categorize.service_config[0].uri
+    body        = base64encode("{\"days\": 7}")
+    headers = {
+      "Content-Type" = "application/json"
+    }
+
+    oidc_token {
+      service_account_email = google_service_account.default.email
+      audience = google_cloudfunctions2_function.categorize.service_config[0].uri
+    }
+  }
+}
+
+resource "google_cloud_scheduler_job" "job_categorize2" {
+  name             = "categorize-book-feeds-daily2"
+  description      = "categorize-book-feeds-daily2"
+  schedule         = "04 1 * * *"
+  time_zone        = "Asia/Tokyo"
+
+  http_target {
+    http_method = "POST"
+    uri         = google_cloudfunctions2_function.categorize.service_config[0].uri
+    body        = base64encode("{\"days\": 14}")
+    headers = {
+      "Content-Type" = "application/json"
+    }
+
+    oidc_token {
+      service_account_email = google_service_account.default.email
+      audience = google_cloudfunctions2_function.categorize.service_config[0].uri
+    }
   }
 }
