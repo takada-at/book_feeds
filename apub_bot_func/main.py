3694631809e1ff4bd447a88b4d9426d5929c9d14
Atsushi Takada <yoru.hikaru@gmail.com>
2023-07-12 02:02:28 +0900

    add cloud run

--- apub_bot_func/main.py
+++ apub_bot_func/main.py
@@ -35,6 +35,7 @@ def hook():
     data = request.json
     logger.info(data)
     ap_logic.create_note(data["content"])
+    return Response(json.dump({"result": "ok"}), headers={'Content-Type': 'application/json'})
 
 
 @app.route("/inbox", methods=["GET", "POST"])
