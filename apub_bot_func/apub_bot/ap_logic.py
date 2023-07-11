0927e042bf94adc71a80f779e395de41b607aa1d
Atsushi Takada <yoru.hikaru@gmail.com>
2023-07-12 00:03:38 +0900

    add cloud run

--- apub_bot_func/apub_bot/ap_logic.py
+++ apub_bot_func/apub_bot/ap_logic.py
@@ -42,7 +42,7 @@ def send_note(follower, create_activity):
     actor = follower["actor"]
     actor_data = get_actor_data(actor)
     netloc = urlparse(actor_data["inbox"])
-    digest = hashlib.sha256(json.dumps(create_activity).encode("utf-8")).digest()
+    digest = hashlib.sha256(json.dumps(create_activity).encode("utf-8")).hexdigest()
     headers = {
         "Host": netloc.hostname,
         "Date": ap_object.get_now(),
@@ -86,7 +86,7 @@ def accept_follow(actor_data: Dict, request_data: Dict):
     request_json = ap_object.get_accept(request_data)
     # sign header
     netloc = urlparse(actor_data["inbox"])
-    digest = hashlib.sha256(json.dumps(request_json).encode("utf-8")).digest()
+    digest = hashlib.sha256(json.dumps(request_json).encode("utf-8")).hexdigest()
     headers = {
         "Host": netloc.hostname,
         "Date": ap_object.get_now(),
